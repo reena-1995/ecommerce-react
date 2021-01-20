ddad1f0a08bfbc2d34693f76534207bb38d3f3c8
reena-19955 <reenasharma.se@gmail.com>
2021-01-20 22:05:13 +0530

    fetching code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
-import {Products,Navbar,Cart} from '../src/Components';
+import {Products,Navbar,Cart,Loginform} from '../src/Components';
 import { commerce } from './lib/commerce';
+import ReactNotification from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
+import { store } from 'react-notifications-component';
 
 
 const App = () => {
   const [products,setProducts] = useState([]);
   const [cart, setCart]        = useState("");
+  const [logIn,setLogin]       = useState("");
   
   const fetchProducts = async () => {
     const {data}= await commerce.products.list();
@@ -23,6 +27,35 @@ const App = () => {
     const item = await commerce.cart.add(productId,quantity);
     setCart(item.cart);
   }
+  
+  const handleLogin = async (phone_number,device_token,device_type) =>{
+    try {
+      const response = await axios.post( "http://localhost/lunchboxOwebest/lunchbox/api/v1/login",{
+        phone_number:phone_number,
+        device_token:device_token,
+        device_type:device_type
+      });
+      console.log(response.data.status);
+      store.addNotification({
+        title: "Wonderful!",
+        message: response.data.msg,
+        type: response.data.status,
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 5000,
+          onScreen: true
+        }
+      });
+      setLogin(response) 
+    } catch (error) {
+      console.log(error);
+    }
+     
+  }
+ 
  
   useEffect(() => {
     fetchProducts();
@@ -32,15 +65,20 @@ const App = () => {
 
   
   return (
+    
     <Router>
       <div>
-      <Navbar cartCount={cart.total_items}/>
+      <ReactNotification />
+      <Navbar cartCount={cart.total_items} loginButton={handleLogin}/>
        <Switch>
         <Route exact path="/">
             <Products products={products} addToCart={handleCart}/>
         </Route>
         <Route exact path="/cart">
             <Cart cart={cart}/>
+        </Route>
+        <Route exact path="/login">
+            <Loginform/>
         </Route>
       </Switch>
       </div>
